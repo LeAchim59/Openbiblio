84aa2c2e19bad1c19dd3c30deb71c50095bddff4
LeAchim59 <78092540+LeAchim59@users.noreply.github.com>
2024-04-02 23:33:42 +0200

    Delete 0.8.1/sql directory

--- 0.8.1/sql/location_option_dm.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists %prfx%location_option_dm;
-create table %prfx%location_option_dm (
-  code char(4) primary key
-  ,description varchar(40) not null
-  ,default_flg char(1) not null
-)
-  ENGINE=MyISAM
-;
